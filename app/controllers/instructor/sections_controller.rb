2b157d9513dfb46c9fa2bbf5e7ef23ac479a70fe
dbielza <dbielza@ryerson.ca>
2019-09-26 02:17:21 +0000

    Section modal

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,13 +1,30 @@
 class Instructor::SectionsController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_authorized_for_current_course
+  before_action :require_authorized_for_current_course, only: [:create]
+  before_action :require_authorized_for_current_section, only: [:update]
 
   def create
     @section = current_course.sections.create(section_params)
     redirect_to instructor_course_path(current_course)
   end
 
+  def update
+    current_section.update_attributes(section_params)
+    render plain: 'ok!'
+  end
+
   private
+
+  def require_authorized_for_current_section
+    if current_section.course.user != current_user
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  def current_section
+    @current_section ||= Section.find(params[:id])
+  end
+
 
   def require_authorized_for_current_course
     if current_course.user != current_user
@@ -15,12 +32,16 @@ class Instructor::SectionsController < ApplicationController
     end
   end
 
-  helper_method :current_course
-  def current_course
+helper_method :current_course
+def current_course
+  if params[:course_id]
     @current_course ||= Course.find(params[:course_id])
+  else
+    current_section.course
   end
+end
 
   def section_params
-    params.require(:section).permit(:title)
+    params.require(:section).permit(:title, :row_order_position)
   end
 end
